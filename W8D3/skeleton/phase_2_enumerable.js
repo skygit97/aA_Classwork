996fb0ed812f661bfeb930df35e6b5467239a633
Ali Ibsin <ibsin.a1996@gmail.com>
2021-03-10 16:14:06 -0800

    finish enumerables

---
+++ W8D3/skeleton/phase_2_enumerable.js
@@ -0,0 +1,48 @@
+Array.prototype.myEach = function(funct) {
+  for (let i = 0; i < this.length; i++) {
+    funct(this[i])
+  }
+};
+
+function mult(n) {
+  return n*2;
+}
+
+array = [1,2,3];
+// array.myEach((ele) => {console.log(ele*2)});
+// array.myEach(ele => console.log(ele*2))
+
+Array.prototype.myMap = function(funct) {
+  let arr = [];
+  this.myEach(ele => arr.push(funct(ele)));
+  return arr;
+};
+
+
+// array.myMap((ele) => {console.log(ele*0)});
+
+
+Array.prototype.myReduce = function(funct, initialValue) {
+
+  // check if no initialValue, then this[0]
+  let arr = this;
+
+  if (initialValue === undefined) {
+    initialValue = arr[0];
+    arr.shift();
+  }
+
+  let final = initialValue;
+  arr.myEach(ele => final = funct(final, ele));
+
+  return final;
+
+};
+
+// console.log([1, 2, 3].myReduce(function(acc, el) {
+//   return acc + el;
+// }));
+
+// console.log([1, 2, 3].myReduce(function(acc, el) {
+//   return acc + el;
+// }, 25));
