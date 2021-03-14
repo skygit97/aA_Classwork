6174c5fe37d2ce5dc60e72035b5ce84f8ed352b9
skumtong <sky_kumtong@yahoo.com>
2021-03-13 18:57:58 -0800

    Finish phase 3

--- W8D3/intro_js/phase_3_iteration.js
+++ W8D3/intro_js/phase_3_iteration.js
@@ -1,29 +1,52 @@
 Array.prototype.bubbleSort = function() {
   let sorted = false;
+
   while (!sorted) {
-    sorted = true;
+    sorted = true; 
+
     for (let i = 0; i < this.length - 1; i++) {
-      if (this[i + 1] < this[i]) {
+      if ((this[i]) > (this[i + 1])) {
         [this[i], this[i + 1]] = [this[i + 1], this[i]];
         sorted = false;
       }
     }
   }
+
   return this;
 };
 
-
-array = [2, 5, 7, 1, 0];
+const array = [2, 5, 7, 1, 0];
 console.log(array.bubbleSort());
 
-String.prototype.subString = function() {
-  let arr = [];
-  for (let i = 0; i < this.length; i++) {
-    for (let j = i + 1; j <= this.length; j++) {
-      arr.push(this.slice(i, j));
+Array.prototype.bubbleSort2 = function(cb) {
+  let sorted = false;
+
+  while (!sorted) {
+    sorted = true;
+
+    for (let i = 0; i < this.length - 1; i++) {
+      if ( cb(this[i]) > cb(this[i + 1]) ) {
+        [this[i], this[i + 1]] = [this[i + 1], this[i]];
+        sorted = false;
+      }
     }
   }
-  return arr;
+
+  return this;
 };
 
-console.log("hello world".subString());
+console.log(array.bubbleSort2(el => el ** -1));
+
+String.prototype.subStrings = function() {
+  let subs = [];
+
+  for (let i = 0; i < this.length; i++) {
+    for (let j = i + 1; j <= this.length; j++) {
+      subs.push(this.slice(i, j));
+    }
+  }
+
+  return subs;
+};
+
+console.log("hello world".subStrings());
