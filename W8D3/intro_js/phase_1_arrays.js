15c232026311be91c0fac658be2d263b49ddae48
skumtong <sky_kumtong@yahoo.com>
2021-03-13 17:27:42 -0800

    Finish phase1

--- W8D3/intro_js/phase_1_arrays.js
+++ W8D3/intro_js/phase_1_arrays.js
@@ -1,6 +1,5 @@
 Array.prototype.uniq = function() {
   let arr = [];
-
   this.forEach(el => {
     if (!arr.includes(el)) {
       arr.push(el);
@@ -10,11 +9,10 @@ Array.prototype.uniq = function() {
   return arr;
 };
 
-console.log([1,2,2,3,3,3].uniq());
+// console.log([1,2,2,3,3,3].uniq());
 
 Array.prototype.twoSum = function() {
   let pairs = [];
-
   for (let i = 0; i < this.length; i++) {
     for (let j = i + 1; j < this.length; j++) {
       if (this[i] + this[j] === 0) {
@@ -26,26 +24,24 @@ Array.prototype.twoSum = function() {
   return pairs;
 };
 
-
-
-console.log([0,2,3,-2,3].twoSum());
+// console.log([0,2,3,-2,3].twoSum());
 
 Array.prototype.transpose = function() {
-  let length = this.length;
-  let width = this[0].length;
+  const length = this[0].length;
+  const width = this.length;
 
-  let arr1 = [];
-  for(let i = 0; i < width; i++) {
-
-    let arr2 = [];
-    for(let j = 0; j < length; j++) {
-      arr2.push(this[j][i]);
+  let transposed = [];
+  for (i = 0; i < length; i++) {
+    let row = [];
+    for (j = 0; j < width; j++) {
+      row.push(this[j][i]);
     }
-    arr1.push(arr2);
+    transposed.push(row);
   }
-  return arr1;
+
+  return transposed;
 };
 
-let variable = [[1, 2, 3], [4, 5, 6]];
-console.log(variable.transpose());
-//[1, 2, 3], [4, 5, 6] => [1, 4], [2, 5], [3, 6]
+let ex = [[1, 2, 3], [4, 5, 6]];
+console.log(ex.transpose());
+// [1, 2, 3], [4, 5, 6] => [1, 4], [2, 5], [3, 6]
