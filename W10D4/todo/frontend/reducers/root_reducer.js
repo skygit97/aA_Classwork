de290ad66c89b28c03810195e7cebddce40ddb34
Khoa Nguyen <nkhoa0101@yahoo.com>
2021-03-25 15:28:48 -0700

    Complete phase 3

--- W10D4/todo/frontend/reducers/root_reducer.js
+++ W10D4/todo/frontend/reducers/root_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { todosReducer } from './todo_reducer';
+import { stepsReducer } from './step_reducer';
 
 const rootReducer = combineReducers({
-  todo: todosReducer
+  todo: todosReducer,
+  step: stepsReducer
 })
 
 export default rootReducer;
--- W10D4/todo/frontend/todo_redux.jsx
+++ W10D4/todo/frontend/todo_redux.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
-import {receiveTodo, receiveTodos, removeTodo } from "./actions/todo_actions";
+import { receiveTodo, receiveTodos, removeTodo } from "./actions/todo_actions";
+import { receiveStep, receiveSteps, removeStep } from "./actions/step_actions";
 
 document.addEventListener("DOMContentLoaded", () => {
   const content = document.getElementById("content");
@@ -10,5 +11,9 @@ document.addEventListener("DOMContentLoaded", () => {
   window.receiveTodo = receiveTodo;
   window.receiveTodos = receiveTodos;
   window.removeTodo = removeTodo;
+  window.receiveStep = receiveStep;
+  window.receiveSteps = receiveSteps;
+  window.removeStep = removeStep;
+  
   ReactDOM.render(<h1>Todos App</h1>, content);
 });
