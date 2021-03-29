b440065b418915f7378b9936a6b102b1b2865f78
Khoa Nguyen <nkhoa0101@yahoo.com>
2021-03-29 15:51:51 -0700

    Set up backend

--- W10D4/todo/frontend/reducers/selectors.js
+++
@@ -1,6 +0,0 @@
-export const allTodos = ({ todos }) => (
-  // Object.keys(todo).map(id => todo[id])
-  Object.values(todos)
-);
-
-//export default allTodos;
--- W10D4/todos_app/frontend/components/todo_list/todo_list_item.jsx
+++ W10D4/todos_app/frontend/components/todo_list/todo_list_item.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
 
-export const TodoListItem = ({todo}) => {
-  return (
-    <li key={todo.id}>{todo.title}</li>
-  )
+export class TodoListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      done: this.props.todo.done
+    }
+
+    console.log(this.state);
+    this.updateDone = this.updateDone.bind(this);
+    // this.removeTodo = this.removeTodo.bind(this);
+  }
+
+  updateDone(e) {
+    e.preventDefault();
+    const toggleTodo = Object.assign({}, this.props.todo, {done: !this.props.todo.done});
+    this.props.receiveTodo(toggleTodo);
+  }
+
+  render() {
+    return (
+      <li key={this.props.todo.id}>
+        {this.props.todo.title}
+        <button onClick={this.updateDone}>{this.props.todo.done ? 'Undo' : 'Done'}</button>
+        <button onClick={this.props.removeTodo}>Delete</button>
+      </li>
+    )
+  }
 } 
